c5a960c4edfd465a742b129d6acbf9182ff4932d
Le Vivilet <levivilet@pm.me>
2026-02-05 20:36:14 +0100

    fixfix

--- packages/preview-worker/src/parts/SaveState/SaveState.ts
+++ packages/preview-worker/src/parts/SaveState/SaveState.ts
@@ -2,9 +2,7 @@ import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import type { SavedState } from '../SavedState/SavedState.ts'
 
 export const saveState = (state: PreviewState): SavedState => {
-  const { statusBarItemsLeft, statusBarItemsRight } = state
   return {
-    itemsLeft: statusBarItemsLeft,
-    itemsRight: statusBarItemsRight,
+    x: 0,
   }
 }
--- packages/preview-worker/src/parts/SavedState/SavedState.ts
+++ packages/preview-worker/src/parts/SavedState/SavedState.ts
@@ -1,6 +1,3 @@
-import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
-
 export interface SavedState {
-  readonly itemsLeft: readonly StatusBarItem[]
-  readonly itemsRight: readonly StatusBarItem[]
+  readonly x: any
 }
