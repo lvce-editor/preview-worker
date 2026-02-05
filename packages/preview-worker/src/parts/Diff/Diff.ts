47bcbdf9b8123d239b9979b3119c537c502408d0
Le Vivilet <levivilet@pm.me>
2026-02-05 20:32:50 +0100

    fix

--- packages/preview-worker/src/parts/Diff/Diff.ts
+++ packages/preview-worker/src/parts/Diff/Diff.ts
@@ -1,7 +1,6 @@
-import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import * as DiffModules from '../DiffModules/DiffModules.ts'
 
-export const diff = (oldState: readonly any[], newState: readonly any[]): readonly number[] => {
+export const diff = (oldState: any, newState: any): readonly number[] => {
   const diffResult: number[] = []
   for (let i = 0; i < DiffModules.modules.length; i++) {
     const fn = DiffModules.modules[i]
