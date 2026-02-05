505bdf9f8f0eb6a29aa70df1e62dd9f691b34c58
Le Vivilet <levivilet@pm.me>
2026-02-05 21:20:10 +0100

    refactor: update commandMap to use Preview namespace

--- packages/preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,5 @@
 import { terminate } from '@lvce-editor/viewlet-registry'
-import * as StatusBar from '../Create/Create.ts'
+import * as Preview from '../Create/Create.ts'
 import { diff2 } from '../Diff2/Diff2.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import { getCommandIds, wrapCommand, wrapGetter } from '../PreviewStates/PreviewStates.ts'
@@ -9,13 +9,13 @@ import { resize } from '../Resize/Resize.ts'
 import { saveState } from '../SaveState/SaveState.ts'
 
 export const commandMap = {
-  'StatusBar.create': StatusBar.create,
-  'StatusBar.diff2': diff2,
-  'StatusBar.getCommandIds': getCommandIds,
-  'StatusBar.loadContent': wrapCommand(LoadContent.loadContent),
-  'StatusBar.render2': render2,
-  'StatusBar.renderEventListeners': renderEventListeners,
-  'StatusBar.resize': wrapCommand(resize),
-  'StatusBar.saveState': wrapGetter(saveState),
-  'StatusBar.terminate': terminate,
+  'Preview.create': Preview.create,
+  'Preview.diff2': diff2,
+  'Preview.getCommandIds': getCommandIds,
+  'Preview.loadContent': wrapCommand(LoadContent.loadContent),
+  'Preview.render2': render2,
+  'Preview.renderEventListeners': renderEventListeners,
+  'Preview.resize': wrapCommand(resize),
+  'Preview.saveState': wrapGetter(saveState),
+  'Preview.terminate': terminate,
 }
