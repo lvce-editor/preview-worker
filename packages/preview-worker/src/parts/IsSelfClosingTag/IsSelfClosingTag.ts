456c399e1bb3484ac05de4e012449accabdddc18
Le Vivilet <levivilet@pm.me>
2026-02-06 00:28:32 +0100

    fix

--- packages/preview-worker/src/parts/IsSelfClosingTag/IsSelfClosingTag.ts
+++ packages/preview-worker/src/parts/IsSelfClosingTag/IsSelfClosingTag.ts
@@ -3,6 +3,8 @@ import * as ElementTags from '../ElementTags/ElementTags.ts'
 export const isSelfClosingTag = (tag: string): boolean => {
   switch (tag) {
     case ElementTags.Img:
+    case ElementTags.Br:
+    case ElementTags.Hr:
       return true
     default:
       return false
