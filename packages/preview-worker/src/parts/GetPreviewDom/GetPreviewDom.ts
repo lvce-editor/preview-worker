632b07c0c2bd890f967d59ea8ba08431093b393f
Le Vivilet <levivilet@pm.me>
2026-02-05 22:04:14 +0100

    feat: implement getEmptyPreviewDom function and associated tests (#19)

--- packages/preview-worker/src/parts/GetPreviewDom/GetPreviewDom.ts
+++ packages/preview-worker/src/parts/GetPreviewDom/GetPreviewDom.ts
@@ -1,27 +1,22 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
-
-const getEmptyPreviewDom = (): readonly any[] => {
-  return [
-    {
-      childCount: 1,
-      className: 'Viewlet Preview',
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 1,
-      type: VirtualDomElements.H1,
-    },
-    {
-      text: 'No URI has been specified',
-      type: VirtualDomElements.Text,
-    },
-  ]
-}
+import { getEmptyPreviewDom } from '../GetEmptyPreviewDom/GetEmptyPreviewDom.ts'
 
 export const getPreviewDom = (state: PreviewState): readonly any[] => {
   if (!state.uri) {
     return getEmptyPreviewDom()
+  }
+
+  // If parsedDom is available, render it as children of the wrapper
+  if (state.parsedDom && state.parsedDom.length > 0) {
+    return [
+      {
+        childCount: 1, // TODO
+        className: 'Viewlet Preview',
+        type: VirtualDomElements.Div,
+      },
+      ...state.parsedDom,
+    ]
   }
 
   return [
