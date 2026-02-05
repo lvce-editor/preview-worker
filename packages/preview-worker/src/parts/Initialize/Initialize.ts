313b0c4ad55b56443d656b204f5ea5b378641f59
Le Vivilet <levivilet@pm.me>
2026-02-05 20:28:01 +0100

    unused

--- packages/preview-worker/src/parts/Initialize/Initialize.ts
+++
@@ -1,7 +0,0 @@
-import { ExtensionHost } from '@lvce-editor/rpc-registry'
-import { createExtensionHostRpc } from '../CreateExtensionHostRpc/CreateExtensionHostRpc.ts'
-
-export const initialize = async (): Promise<void> => {
-  const rpc = await createExtensionHostRpc()
-  ExtensionHost.set(rpc)
-}
