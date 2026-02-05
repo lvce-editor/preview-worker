ceec09d0fccd62b6eae3e35d9b7789b273c09969
Le Vivilet <levivilet@pm.me>
2026-02-05 21:23:07 +0100

    fix: preview states (#5)

--- packages/preview-worker/src/parts/PreviewStates/PreviewStates.ts
+++ packages/preview-worker/src/parts/PreviewStates/PreviewStates.ts
@@ -1,39 +1,4 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
+import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
 
-const states = new Map<number, { newState: PreviewState; oldState: PreviewState }>()
-
-export const set = (uid: number, newState: PreviewState, oldState: PreviewState): void => {
-  states.set(uid, { newState, oldState })
-}
-
-export const get = (uid: number): { newState: PreviewState; oldState: PreviewState } => {
-  const state = states.get(uid)
-  if (!state) {
-    throw new Error(`State not found for uid ${uid}`)
-  }
-  return state
-}
-
-export const registerCommands = (commandMap: Record<string, unknown>): void => {
-  // Register commands if needed
-}
-
-export const getCommandIds = (): readonly string[] => {
-  return []
-}
-
-export const wrapCommand = (fn: (state: PreviewState, ...args: unknown[]) => unknown) => {
-  return (uid: number, ...args: unknown[]) => {
-    const { newState } = get(uid)
-    return fn(newState, ...args)
-  }
-}
-
-export const wrapGetter = (fn: (state: PreviewState) => unknown) => {
-  return (uid: number) => {
-    const { newState } = get(uid)
-    return fn(newState)
-  }
-}
+export const { get, getCommandIds, registerCommands, set, wrapCommand, wrapGetter } = ViewletRegistry.create<PreviewState>()
