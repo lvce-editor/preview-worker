7a5a48301e56e313fa38a838c94fdaf76e0cd265
Le Vivilet <levivilet@pm.me>
2026-02-05 21:08:48 +0100

    typo

--- packages/preview-worker/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/preview-worker/src/parts/RenderIncremental/RenderIncremental.ts
@@ -3,7 +3,7 @@ import { diffTree } from '@lvce-editor/virtual-dom-worker'
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import { renderItems } from '../RenderItems/RenderItems.ts'
 
-export const renderIcremental = (oldState: PreviewState, newState: PreviewState): any => {
+export const renderIncremental = (oldState: PreviewState, newState: PreviewState): any => {
   const oldDom = renderItems(oldState, oldState)[2]
   const newDom = renderItems(newState, newState)[2]
   const patches = diffTree(oldDom, newDom)
