b3ad0b136d0942fc4c56454c3b0efec4e71cb5fe
Le Vivilet <levivilet@pm.me>
2026-02-05 22:22:42 +0100

    feat: implement handleFileEdited function and associated tests; enhance PreviewState and updateContent for improved error handling (#21)

--- packages/preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,7 @@
 import { terminate } from '@lvce-editor/viewlet-registry'
 import * as Preview from '../Create/Create.ts'
 import { diff2 } from '../Diff2/Diff2.ts'
+import { handleFileEdited } from '../HandleFileEdited/HandleFileEdited.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import { getCommandIds, wrapCommand, wrapGetter } from '../PreviewStates/PreviewStates.ts'
 import { render2 } from '../Render2/Render2.ts'
@@ -13,6 +14,7 @@ export const commandMap = {
   'Preview.create': Preview.create,
   'Preview.diff2': diff2,
   'Preview.getCommandIds': getCommandIds,
+  'Preview.handleFileEdited': wrapCommand(handleFileEdited),
   'Preview.loadContent': wrapCommand(LoadContent.loadContent),
   'Preview.render2': render2,
   'Preview.renderEventListeners': renderEventListeners,
