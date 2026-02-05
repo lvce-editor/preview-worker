135f2b4e1b80a85ac8df906d4fd03449cdf39f70
Le Vivilet <levivilet@pm.me>
2026-02-05 22:02:03 +0100

    fix: enhance isEqual function to compare content and parsedDom properties

--- packages/preview-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/preview-worker/src/parts/DiffItems/DiffItems.ts
@@ -1,5 +1,10 @@
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
 
 export const isEqual = (oldState: PreviewState, newState: PreviewState): boolean => {
-  return oldState.warningCount === newState.warningCount && oldState.initial === newState.initial
+  return (
+    oldState.warningCount === newState.warningCount &&
+    oldState.initial === newState.initial &&
+    oldState.content === newState.content &&
+    oldState.parsedDom === newState.parsedDom
+  )
 }
