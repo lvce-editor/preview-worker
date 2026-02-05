0f65d241f64d0967056dc48a3e3ca9bd8a875bd5
Le Vivilet <levivilet@pm.me>
2026-02-05 21:44:57 +0100

    refactor: extract empty preview DOM creation into a separate function

--- packages/preview-worker/src/parts/GetPreviewDom/GetPreviewDom.ts
+++ packages/preview-worker/src/parts/GetPreviewDom/GetPreviewDom.ts
@@ -1,23 +1,27 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
 
+const getEmptyPreviewDom = (): readonly any[] => {
+  return [
+    {
+      childCount: 1,
+      className: 'Viewlet Preview',
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 1,
+      type: VirtualDomElements.H1,
+    },
+    {
+      text: 'No URI has been specified',
+      type: VirtualDomElements.Text,
+    },
+  ]
+}
+
 export const getPreviewDom = (state: PreviewState): readonly any[] => {
   if (!state.uri) {
-    return [
-      {
-        childCount: 1,
-        className: 'Viewlet Preview',
-        type: VirtualDomElements.Div,
-      },
-      {
-        childCount: 1,
-        type: VirtualDomElements.H1,
-      },
-      {
-        text: 'No URI has been specified',
-        type: VirtualDomElements.Text,
-      },
-    ]
+    return getEmptyPreviewDom()
   }
 
   return [
