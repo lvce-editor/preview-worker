7be0161736b3a71a552f7b47d26a76d752758cef
Le Vivilet <levivilet@pm.me>
2026-02-05 20:37:11 +0100

    fixfix

--- packages/preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,11 +1,6 @@
 import { terminate } from '@lvce-editor/viewlet-registry'
 import * as StatusBar from '../Create/Create.ts'
 import { diff2 } from '../Diff2/Diff2.ts'
-import * as HandleClick from '../HandleClick/HandleClick.ts'
-import { initialize } from '../Initialize/Initialize.ts'
-import * as ItemLeftUpdate from '../ItemLeftUpdate/ItemLeftUpdate.ts'
-import * as ItemRightCreate from '../ItemRightCreate/ItemRightCreate.ts'
-import * as ItemRightUpdate from '../ItemRightUpdate/ItemRightUpdate.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import { getCommandIds, wrapCommand, wrapGetter } from '../PreviewStates/PreviewStates.ts'
 import { render2 } from '../Render2/Render2.ts'
@@ -17,11 +12,6 @@ export const commandMap = {
   'StatusBar.create': StatusBar.create,
   'StatusBar.diff2': diff2,
   'StatusBar.getCommandIds': getCommandIds,
-  'StatusBar.handleClick': wrapCommand(HandleClick.handleClick),
-  'StatusBar.initialize': initialize,
-  'StatusBar.itemLeftUpdate': wrapCommand(ItemLeftUpdate.itemLeftUpdate),
-  'StatusBar.itemRightCreate': wrapCommand(ItemRightCreate.itemRightCreate),
-  'StatusBar.itemRightUpdate': wrapCommand(ItemRightUpdate.itemRightUpdate),
   'StatusBar.loadContent': wrapCommand(LoadContent.loadContent),
   'StatusBar.render2': render2,
   'StatusBar.renderEventListeners': renderEventListeners,
--- packages/preview-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/preview-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,17 +1,10 @@
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
-import * as GetStatusBarItems from '../GetStatusBarItems/GetStatusBarItems.ts'
-import * as StatusBarPreferences from '../StatusBarPreferences/StatusBarPreferences.ts'
 
 export const loadContent = async (state: PreviewState): Promise<PreviewState> => {
-  const { assetDir, errorCount, platform, warningCount } = state
-  const statusBarItemsPreference = await StatusBarPreferences.itemsVisible()
-  const statusBarItems = await GetStatusBarItems.getStatusBarItems(statusBarItemsPreference, assetDir, platform, errorCount, warningCount)
   return {
     ...state,
     errorCount: 0,
     initial: false,
-    statusBarItemsLeft: [...statusBarItems],
-    statusBarItemsRight: [],
     warningCount: 0,
   }
 }
