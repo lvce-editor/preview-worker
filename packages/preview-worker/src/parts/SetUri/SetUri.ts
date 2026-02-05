5efdd025415e17ef098911327a28eaa9d100fe15
Le Vivilet <levivilet@pm.me>
2026-02-05 22:02:07 +0100

    feat: update setUri function to read file content and parse HTML into virtual DOM (#16)

--- packages/preview-worker/src/parts/SetUri/SetUri.ts
+++ packages/preview-worker/src/parts/SetUri/SetUri.ts
@@ -1,8 +1,29 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
+import * as ParseHtml from '../ParseHtml/ParseHtml.ts'
 
-export const setUri = (state: PreviewState, uri: string): PreviewState => {
-  return {
-    ...state,
-    uri,
+export const setUri = async (state: PreviewState, uri: string): Promise<PreviewState> => {
+  try {
+    // Read the file content using RendererWorker RPC
+    // @ts-ignore
+    const content = await RendererWorker.readFile(uri)
+
+    // Parse the content into virtual DOM
+    const parsedDom = ParseHtml.parseHtml(content, [])
+
+    return {
+      ...state,
+      content,
+      parsedDom,
+      uri,
+    }
+  } catch {
+    // If file reading fails, return state with just the URI set
+    return {
+      ...state,
+      content: '',
+      parsedDom: [],
+      uri,
+    }
   }
 }
