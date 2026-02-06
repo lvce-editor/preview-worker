2b3c7183b87e67e4913f6d823f0e8d3c44488290
Le Vivilet <levivilet@pm.me>
2026-02-06 23:39:30 +0100

    fix

--- packages/preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -2,9 +2,9 @@ import { terminate } from '@lvce-editor/viewlet-registry'
 import * as Preview from '../Create/Create.ts'
 import { diff2 } from '../Diff2/Diff2.ts'
 import * as HandleClick from '../HandleClick/HandleClick.ts'
-import * as HandleInput from '../HandleInput/HandleInput.ts'
 import { handleEditorChanged } from '../HandleEditorChanged/HandleEditorChanged.ts'
 import { handleFileEdited } from '../HandleFileEdited/HandleFileEdited.ts'
+import * as HandleInput from '../HandleInput/HandleInput.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import { getCommandIds, wrapCommand, wrapGetter } from '../PreviewStates/PreviewStates.ts'
 import { render2 } from '../Render2/Render2.ts'
@@ -20,8 +20,8 @@ export const commandMap = {
   'Preview.diff2': diff2,
   'Preview.getCommandIds': getCommandIds,
   'Preview.handleClick': wrapCommand(HandleClick.handleClick),
-  'Preview.handleInput': wrapCommand(HandleInput.handleInput),
   'Preview.handleFileEdited': wrapCommand(handleFileEdited),
+  'Preview.handleInput': wrapCommand(HandleInput.handleInput),
   'Preview.loadContent': wrapCommand(LoadContent.loadContent),
   'Preview.render2': render2,
   'Preview.renderEventListeners': renderEventListeners,
