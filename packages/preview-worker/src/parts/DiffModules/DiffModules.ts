e96faff6ecf7aaec2441b4594f4cf2c873e33121
Le Vivilet <levivilet@pm.me>
2026-02-06 13:33:20 +0100

    fix

--- packages/preview-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/preview-worker/src/parts/DiffModules/DiffModules.ts
@@ -2,6 +2,6 @@ import * as DiffCss from '../DiffCss/DiffCss.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 
-export const modules = [DiffItems.isEqual, DiffCss.isDiffCss]
+export const modules = [DiffItems.isEqual, DiffCss.iEqual]
 
 export const numbers = [DiffType.RenderIncremental, DiffType.RenderCss]
