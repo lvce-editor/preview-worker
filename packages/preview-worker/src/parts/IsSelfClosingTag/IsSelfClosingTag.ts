39ce5d615f7546b3b4c588b5b78f04478970309d
Le Vivilet <levivilet@pm.me>
2026-02-06 12:08:51 +0100

    fix

--- packages/preview-worker/src/parts/IsSelfClosingTag/IsSelfClosingTag.ts
+++ packages/preview-worker/src/parts/IsSelfClosingTag/IsSelfClosingTag.ts
@@ -2,16 +2,16 @@ import * as ElementTags from '../ElementTags/ElementTags.ts'
 
 export const isSelfClosingTag = (tag: string): boolean => {
   switch (tag.toLowerCase()) {
+    case 'area':
+    case 'base':
+    case 'col':
     case ElementTags.Br:
     case ElementTags.Hr:
     case ElementTags.Img:
     case ElementTags.Input:
-    case 'meta':
-    case 'link':
-    case 'base':
-    case 'area':
-    case 'col':
     case 'embed':
+    case 'link':
+    case 'meta':
     case 'param':
     case 'source':
     case 'track':
