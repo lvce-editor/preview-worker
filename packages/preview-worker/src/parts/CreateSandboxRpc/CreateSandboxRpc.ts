414f1897f5f648e36e1cca1131cf62d464811e4a
Le Vivilet <levivilet@pm.me>
2026-02-08 00:12:42 +0100

    fix

--- packages/preview-worker/src/parts/CreateSandboxRpc/CreateSandboxRpc.ts
+++ packages/preview-worker/src/parts/CreateSandboxRpc/CreateSandboxRpc.ts
@@ -4,7 +4,12 @@ import { RendererWorker } from '@lvce-editor/rpc-registry'
 export const createSandboxRpc = async (): Promise<any> => {
   const sandboxRpc = await LazyTransferMessagePortRpcParent.create({
     commandMap: {},
-    send: (port: any) => RendererWorker.invoke('SendMessagePortToSandboxWorker', port),
+    send: (port: any) =>
+      RendererWorker.invokeAndTransfer(
+        'SendMessagePortToExtensionHostWorker.sendMessagePortToPreviewSandBoxWorker',
+        'SandBox.handleMessagePort',
+        port,
+      ),
   })
   return sandboxRpc
 }
