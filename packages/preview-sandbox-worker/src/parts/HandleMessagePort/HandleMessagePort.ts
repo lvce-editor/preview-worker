aa3c37e0c04662f12878e831382a95dbe3a20c36
Le Vivilet <levivilet@pm.me>
2026-02-08 00:20:39 +0100

    fix

--- packages/preview-sandbox-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/preview-sandbox-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -3,10 +3,7 @@ import { PlainMessagePortRpc } from '@lvce-editor/rpc'
 export const handleMessagePort = async (port: MessagePort): Promise<void> => {
   await PlainMessagePortRpc.create({
     commandMap: {},
-<<<<<<< HEAD
     isMessagePortOpen: true,
-=======
->>>>>>> origin/main
     messagePort: port,
   })
 }
