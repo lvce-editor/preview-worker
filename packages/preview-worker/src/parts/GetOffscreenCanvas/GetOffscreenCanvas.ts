14f8a8fb937998a7e966fc40d324065581fe2dcf
Le Vivilet <levivilet@pm.me>
2026-02-07 11:45:46 +0100

    feat: add executeCallback function and integrate it into commandMap

--- packages/preview-worker/src/parts/GetOffscreenCanvas/GetOffscreenCanvas.ts
+++ packages/preview-worker/src/parts/GetOffscreenCanvas/GetOffscreenCanvas.ts
@@ -1,6 +1,33 @@
+/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
+const callBacks = Object.create(null)
+
+let id = 0
+
+const registerCallback = (): any => {
+  const nextId = id++
+  const { promise, resolve } = Promise.withResolvers<OffscreenCanvas>()
+  callBacks[nextId] = resolve
+  return {
+    id: nextId,
+    promise,
+  }
+}
+
+export const executeCallback = (id: number, offscreenCanvas: OffscreenCanvas): void => {
+  const callback = callBacks[id]
+  if (callback) {
+    callback(offscreenCanvas)
+    delete callBacks[id]
+  } else {
+    console.warn(`[preview-worker] No callback found for id ${id}`)
+  }
+}
+
 export const getOffscreenCanvas = async (canvasId: number): Promise<OffscreenCanvas> => {
-  // @ts-ignore
-  return await RendererWorker.invoke('OffscreenCanvas.create', canvasId)
+  const { id, promise } = registerCallback()
+  await RendererWorker.invoke('OffscreenCanvas.create', canvasId, id)
+  const offscreenCanvas = await promise
+  return offscreenCanvas
 }
