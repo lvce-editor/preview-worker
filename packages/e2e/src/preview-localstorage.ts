c392a7768565bcefb23785925763442558d07c98
Le Vivilet <levivilet@pm.me>
2026-02-08 00:08:29 +0100

    refactor: remove skip export from preview.localstorage test

--- packages/e2e/src/preview-localstorage.ts
+++ packages/e2e/src/preview-localstorage.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'preview.localstorage'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
