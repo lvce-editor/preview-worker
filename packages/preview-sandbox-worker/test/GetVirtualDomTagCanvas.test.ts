9562824d553df939972860c72bec4f2dcf61bdf2
Le Vivilet <levivilet@pm.me>
2026-02-07 23:07:32 +0100

    fix

--- packages/preview-sandbox-worker/test/GetVirtualDomTagCanvas.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@jest/globals'
-import { VirtualDomElements } from '@lvce-editor/constants'
-import * as ElementTags from '../src/parts/ElementTags/ElementTags.ts'
-import { getVirtualDomTag } from '../src/parts/GetVirtualDomTag/GetVirtualDomTag.ts'
-
-test('getVirtualDomTag should return Canvas element for "canvas" tag', () => {
-  const result = getVirtualDomTag(ElementTags.Canvas)
-  expect(result).toBe(VirtualDomElements.Canvas)
-})
