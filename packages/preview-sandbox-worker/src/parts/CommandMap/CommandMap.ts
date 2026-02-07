0461875821afda5c4b6bc64903126f3909f15a62
Le Vivilet <levivilet@pm.me>
2026-02-08 00:13:00 +0100

    feature: add handleMessagePort function and integrate into commandMap

--- packages/preview-sandbox-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/preview-sandbox-worker/src/parts/CommandMap/CommandMap.ts
@@ -3,6 +3,7 @@ import * as HandleClick from '../HandleClick/HandleClick.ts'
 import * as HandleInput from '../HandleInput/HandleInput.ts'
 import * as HandleKeydown from '../HandleKeydown/HandleKeydown.ts'
 import * as HandleKeyup from '../HandleKeyup/HandleKeyup.ts'
+import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import { initializeSandbox } from '../InitializeSandbox/InitializeSandbox.ts'
 
 export const commandMap = {
@@ -10,6 +11,7 @@ export const commandMap = {
   'SandBox.handleInput': HandleInput.handleInput,
   'SandBox.handleKeyDown': HandleKeydown.handleKeydown,
   'SandBox.handleKeyUp': HandleKeyup.handleKeyup,
+  'SandBox.handleMessagePort': handleMessagePort,
   'SandBox.initialize': initializeSandbox,
   'SandBox.terminate': terminate,
 }
