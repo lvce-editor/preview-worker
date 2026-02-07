8c05adab1c4e5f42d56b492084afcc5b3828b06d
Le Vivilet <levivilet@pm.me>
2026-02-07 23:08:51 +0100

    fix

--- packages/preview-sandbox-worker/src/parts/Listen/Listen.ts
+++ packages/preview-sandbox-worker/src/parts/Listen/Listen.ts
@@ -1,18 +1,10 @@
-import { WebWorkerRpcClient, LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
-import { EditorWorker, RendererWorker } from '@lvce-editor/rpc-registry'
+import { WebWorkerRpcClient } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import { registerCommands } from '../PreviewStates/PreviewStates.ts'
 
 export const listen = async (): Promise<void> => {
-  registerCommands(CommandMap.commandMap)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
   RendererWorker.set(rpc)
-
-  const editorRpc = await LazyTransferMessagePortRpcParent.create({
-    commandMap: {},
-    send: (port: any) => RendererWorker.sendMessagePortToEditorWorker(port, 9112),
-  })
-  EditorWorker.set(editorRpc)
 }
