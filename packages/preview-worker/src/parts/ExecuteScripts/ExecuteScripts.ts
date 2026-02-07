6e6b1147e6a13eee4aaa12569e885683d9188bcb
Le Vivilet <levivilet@pm.me>
2026-02-08 00:04:59 +0100

    feature: integrate createLocalStorage into executeScripts for local storage support (#103)

--- packages/preview-worker/src/parts/ExecuteScripts/ExecuteScripts.ts
+++ packages/preview-worker/src/parts/ExecuteScripts/ExecuteScripts.ts
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/no-implied-eval */
 import * as Alert from '../Alert/Alert.ts'
 import { getTopLevelFunctionNames } from '../GetTopLevelFunctionNames/GetTopLevelFunctionNames.ts'
+import { createLocalStorage } from '../LocalStorage/LocalStorage.ts'
 
 export const executeScripts = (window: any, document: any, scripts: readonly string[]): void => {
   window.alert = Alert.alert
   // @ts-ignore
   globalThis.alert = Alert.alert
+  const localStorage = createLocalStorage()
+  // @ts-ignore
+  globalThis.localStorage = localStorage
   // Execute each script with the happy-dom window and document as context
   for (const scriptContent of scripts) {
     try {
