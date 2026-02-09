7f7c7955c791d227842ed418634a7935429d14ac
Le Vivilet <levivilet@pm.me>
2026-02-09 04:20:10 +0100

    unused

--- packages/preview-sandbox-worker/src/parts/PatchCanvasElements/PatchCanvasElements.ts
+++ packages/preview-sandbox-worker/src/parts/PatchCanvasElements/PatchCanvasElements.ts
@@ -18,43 +18,6 @@ const generateCanvasCssRule = (dataUid: string, width: number, height: number): 
   return `[data-id="${dataUid}"] { width: ${width}px; height: ${height}px; }`
 }
 
-const createNewOffscreenCanvas = async (
-  w: number,
-  h: number,
-  element: any,
-  uid: number,
-  onCanvasDimensionsChange?: (element: any, width: number, height: number, cssRule?: string) => Promise<void>,
-): Promise<void> => {
-  if (Number.isNaN(w) || Number.isNaN(h) || w <= 0 || h <= 0) {
-    return
-  }
-
-  const { offscreenCanvas: newOffscreenCanvas } = await getOffscreenCanvas(w, h)
-  const newContext = newOffscreenCanvas.getContext('2d')
-
-  // Update getContext to return the new context
-  element.getContext = (contextType: string): any => {
-    if (contextType === '2d') {
-      return newContext
-    }
-    return undefined
-  }
-
-  // Update the instance in CanvasState
-  const state = CanvasState.get(uid)
-  if (state) {
-    const instanceIndex = state.instances.findIndex((inst: readonly any) => inst.element === element)
-    if (instanceIndex !== -1) {
-      ;(state.instances[instanceIndex] as any).offscreenCanvas = newOffscreenCanvas
-    }
-  }
-
-  // Notify if callback is provided
-  if (onCanvasDimensionsChange) {
-    await onCanvasDimensionsChange(element, w, h)
-  }
-}
-
 export const patchCanvasElements = async (
   document: any,
   uid: number,
