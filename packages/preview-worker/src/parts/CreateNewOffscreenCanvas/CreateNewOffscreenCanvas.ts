88c457257f7a5d0acef7503d7e59ed12e2e7c936
Le Vivilet <levivilet@pm.me>
2026-02-09 04:29:35 +0100

    fix

--- packages/preview-worker/src/parts/CreateNewOffscreenCanvas/CreateNewOffscreenCanvas.ts
+++ packages/preview-worker/src/parts/CreateNewOffscreenCanvas/CreateNewOffscreenCanvas.ts
@@ -3,8 +3,8 @@ export const toNumber = (value: string | number | null): number => {
     return value
   }
   if (typeof value === 'string') {
-    const parsed = parseInt(value, 10)
-    return isNaN(parsed) ? 0 : parsed
+    const parsed = Number.parseInt(value, 10)
+    return Number.isNaN(parsed) ? 0 : parsed
   }
   return 0
 }
