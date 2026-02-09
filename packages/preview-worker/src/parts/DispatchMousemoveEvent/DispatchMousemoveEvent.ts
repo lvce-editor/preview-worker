86437385a53e4def8eeeca5710f777b1c9005bd3
Le Vivilet <levivilet@pm.me>
2026-02-09 15:04:19 +0100

    feat: update mousemove event handling to include clientX and clientY parameters in dispatch and local handlers (#130)

--- packages/preview-worker/src/parts/DispatchMousemoveEvent/DispatchMousemoveEvent.ts
+++ packages/preview-worker/src/parts/DispatchMousemoveEvent/DispatchMousemoveEvent.ts
@@ -1,6 +1,6 @@
 import * as DispatchEvent from '../DispatchEvent/DispatchEvent.ts'
 
-export const dispatchMousemoveEvent = (element: any, window: any): void => {
-  const mousemoveEvent = new window.MouseEvent('mousemove', { bubbles: true })
+export const dispatchMousemoveEvent = (element: any, window: any, clientX: number = 0, clientY: number = 0): void => {
+  const mousemoveEvent = new window.MouseEvent('mousemove', { bubbles: true, clientX, clientY })
   DispatchEvent.dispatchEvent(element, mousemoveEvent)
 }
