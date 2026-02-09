3ec136885537571957e2e8f9eef3194bb31e1ca1
Le Vivilet <levivilet@pm.me>
2026-02-09 15:03:36 +0100

    fix

--- packages/preview-worker/src/parts/DispatchMouseupEvent/DispatchMouseupEvent.ts
+++ packages/preview-worker/src/parts/DispatchMouseupEvent/DispatchMouseupEvent.ts
@@ -1,11 +1,6 @@
 import * as DispatchEvent from '../DispatchEvent/DispatchEvent.ts'
 
-<<<<<<< HEAD
-export const dispatchMouseupEvent = (element: any, window: any): void => {
-  const mouseupEvent = new window.MouseEvent('mouseup', { bubbles: true })
-=======
 export const dispatchMouseupEvent = (element: any, window: any, clientX: number = 0, clientY: number = 0): void => {
   const mouseupEvent = new window.MouseEvent('mouseup', { bubbles: true, clientX, clientY })
->>>>>>> origin/main
   DispatchEvent.dispatchEvent(element, mouseupEvent)
 }
