aff5bb76653ac0d7cde23507f860fa7a86bfbdc9
Le Vivilet <levivilet@pm.me>
2026-02-09 03:41:55 +0100

    fix

--- packages/preview-worker/test/CreateNewOffscreenCanvas.test.ts
+++ packages/preview-worker/test/CreateNewOffscreenCanvas.test.ts
@@ -158,7 +158,11 @@ describe('createNewOffscreenCanvas', () => {
       },
     })
 
-    const callback = async (_element: any, width: number, height: number): Promise<void> => {
+    const callback = async (
+      _element: any,
+      width: number,
+      height: number,
+    ): Promise<void> => {
       callbackSpy.push({ height, width })
     }
 
