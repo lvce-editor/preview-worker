61bf95d36a1b4ca27e6645027db7a449b0bc54f8
Le Vivilet <levivilet@pm.me>
2026-02-10 18:05:47 +0100

    x,y

--- packages/preview-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/preview-worker/src/parts/HandleClick/HandleClick.ts
@@ -1,9 +1,9 @@
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import * as CallAndUpdate from '../CallAndUpdate/CallAndUpdate.ts'
 
-export const handleClick = async (state: PreviewState, hdId: string): Promise<PreviewState> => {
+export const handleClick = async (state: PreviewState, hdId: string, clientX: number, clientY: number): Promise<PreviewState> => {
   if (!hdId) {
     return state
   }
-  return CallAndUpdate.callAndUpdate(state, 'SandBox.handleClick', hdId)
+  return CallAndUpdate.callAndUpdate(state, 'SandBox.handleClick', hdId, clientX, clientY)
 }
