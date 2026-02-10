c80ab91a3849bf9317b4941d36373fef8b2ec90d
Le Vivilet <levivilet@pm.me>
2026-02-10 15:27:33 +0100

    fix

--- packages/preview-worker/src/parts/DispatchClickEvent/DispatchClickEvent.ts
+++
@@ -1,6 +0,0 @@
-import * as DispatchEvent from '../DispatchEvent/DispatchEvent.ts'
-
-export const dispatchClickEvent = (element: any, window: any, clientX: number = 0, clientY: number = 0): void => {
-  const clickEvent = new window.MouseEvent('click', { bubbles: true, clientX, clientY })
-  DispatchEvent.dispatchEvent(element, clickEvent)
-}
