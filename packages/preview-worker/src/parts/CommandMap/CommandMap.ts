37ad3600b14b7a9a978173ddf851aa08b5d09f4b
Le Vivilet <levivilet@pm.me>
2026-02-10 17:55:04 +0100

    add rerender

--- packages/preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -15,7 +15,7 @@ import * as LoadContent from '../LoadContent/LoadContent.ts'
 import { getCommandIds, wrapCommand, wrapGetter } from '../PreviewStates/PreviewStates.ts'
 import { render2 } from '../Render2/Render2.ts'
 import { renderEventListeners } from '../RenderEventListeners/RenderEventListeners.ts'
-import { rerender } from '../Rerender/Rerender.ts'
+import { rerender, triggerRerender } from '../Rerender/Rerender.ts'
 import { resize } from '../Resize/Resize.ts'
 import { saveState } from '../SaveState/SaveState.ts'
 import { setUri } from '../SetUri/SetUri.ts'
@@ -43,4 +43,5 @@ export const commandMap = {
   'Preview.saveState': wrapGetter(saveState),
   'Preview.setUri': wrapCommand(setUri),
   'Preview.terminate': terminate,
+  'Preview.triggerRerender': wrapCommand(triggerRerender),
 }
