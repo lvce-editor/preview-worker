4234745714160a1d1c06d209f0a11ca773fa4eff
Le Vivilet <levivilet@pm.me>
2026-02-18 23:03:25 +0100

    fix

--- packages/preview-worker/test/PatchCanvasElements.test.ts
+++ packages/preview-worker/test/PatchCanvasElements.test.ts
@@ -228,7 +228,7 @@ test.skip('patchCanvasElements callback should be called on dimension changes', 
   // Callback should have been called at least once
   expect(changes.length).toBeGreaterThan(0)
   // Last change should be width=200, height=150
-  expect(changes[changes.length - 1]).toEqual({ height: 150, width: 200 })
+  expect(changes.at(-1)).toEqual({ height: 150, width: 200 })
 })
 
 test('patchCanvasElements should set data-id attribute on canvas elements', async () => {
@@ -278,7 +278,7 @@ test.skip('patchCanvasElements callback should include cssRule parameter on dime
 
   // The callback should have been called with a CSS rule
   expect(changes.length).toBeGreaterThan(0)
-  expect(changes[changes.length - 1].cssRule).toBeDefined()
-  expect(changes[changes.length - 1].cssRule).toContain(`[data-id="${dataUid}"]`)
-  expect(changes[changes.length - 1].cssRule).toContain('width: 200px')
+  expect(changes.at(-1).cssRule).toBeDefined()
+  expect(changes.at(-1).cssRule).toContain(`[data-id="${dataUid}"]`)
+  expect(changes.at(-1).cssRule).toContain('width: 200px')
 })
