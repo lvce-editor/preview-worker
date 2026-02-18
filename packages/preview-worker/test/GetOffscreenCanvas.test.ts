094d8dbd4ec41e56be3460f8ec35de47a6b04d0a
Le Vivilet <levivilet@pm.me>
2026-02-18 23:06:04 +0100

    fix

--- packages/preview-worker/test/GetOffscreenCanvas.test.ts
+++ packages/preview-worker/test/GetOffscreenCanvas.test.ts
@@ -1,45 +1,58 @@
-import { expect, test } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { getOffscreenCanvas, executeCallback } from '../src/parts/GetOffscreenCanvas/GetOffscreenCanvas.ts'
 
-test('getOffscreenCanvas should invoke RendererWorker with correct parameters', async () => {
-  const mockOffscreenCanvas = {} as OffscreenCanvas
-  const mockCanvasId = 42
-  using mockRpc = RendererWorker.registerMockRpc({
-    'OffscreenCanvas.createForPreview': (id: number) => {
-      executeCallback(id, mockOffscreenCanvas, mockCanvasId)
-    },
-  })
+const createState = () => {
+  const sandboxRpc = {
+    invokeAndTransfer: () => {},
+  }
+  const state = {
+    sandboxRpc,
+  }
+  return {
+    state,
+    sandboxRpc,
+  }
+}
 
-  const result = await getOffscreenCanvas(0, 0)
+test('getOffscreenCanvas should invoke RendererWorker with correct parameters', async () => {
+  using mockRpc = RendererWorker.registerMockRpc({
+    'OffscreenCanvas.createForPreview': () => {},
+  })
+  const { state } = createState()
+
+  await getOffscreenCanvas(state as any, 0, 0, 0)
 
   expect(mockRpc.invocations).toEqual([['OffscreenCanvas.createForPreview', 0, 0, 0]])
-  expect(result).toEqual({ canvasId: mockCanvasId, offscreenCanvas: mockOffscreenCanvas })
 })
 
-test('getOffscreenCanvas should return the OffscreenCanvas from RendererWorker', async () => {
-  const mockCanvas = {} as OffscreenCanvas
-  const mockCanvasId = 10
+test('getOffscreenCanvas should store callback rpc and execute callback', async () => {
+  const { state, sandboxRpc } = createState()
+  const invokeAndTransfer = jest.fn()
+  sandboxRpc.invokeAndTransfer = invokeAndTransfer
+
   using mockRpc = RendererWorker.registerMockRpc({
     'OffscreenCanvas.createForPreview': (id: number) => {
-      executeCallback(id, mockCanvas, mockCanvasId)
+      executeCallback(id, 'arg-1', 'arg-2')
     },
   })
 
-  const result = await getOffscreenCanvas(100, 200)
+  await getOffscreenCanvas(state as any, 1, 100, 200)
 
   expect(mockRpc.invocations).toEqual([['OffscreenCanvas.createForPreview', 1, 100, 200]])
-  expect(result).toEqual({ canvasId: mockCanvasId, offscreenCanvas: mockCanvas })
+  expect(invokeAndTransfer).toHaveBeenCalledTimes(1)
+  expect(invokeAndTransfer).toHaveBeenCalledWith('SandBox.executeCallback', 1, 'arg-1', 'arg-2')
 })
 
 test('getOffscreenCanvas should propagate errors from RendererWorker', async () => {
   const testError = new Error('Failed to create OffscreenCanvas')
+  const { state } = createState()
   using mockRpc = RendererWorker.registerMockRpc({
     'OffscreenCanvas.createForPreview': () => {
       throw testError
     },
   })
 
-  await expect(getOffscreenCanvas(0, 0)).rejects.toThrow(testError)
+  await expect(getOffscreenCanvas(state as any, 2, 0, 0)).rejects.toThrow(testError)
   expect(mockRpc.invocations).toEqual([['OffscreenCanvas.createForPreview', 2, 0, 0]])
 })
