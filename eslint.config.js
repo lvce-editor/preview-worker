20721ff1464d3925beb7bebf8b5ff7bf08d02a4f
Le Vivilet <levivilet@pm.me>
2026-02-18 23:38:52 +0100

    feat: enhance TypeScript configuration by adding new compiler options and integrating ESLint plugins (#144)

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,14 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
+import * as tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
 
-export default [...config.default, ...actions.default]
+export default [
+  ...config.default,
+  ...actions.default,
+  ...tsconfig.default,
+  {
+    rules: {
+      'tsconfig/dont-skip-lib-check': 'off',
+    },
+  },
+]
