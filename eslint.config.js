dd51c6c6141ee0a8f52b1b34702aba865a7adf2b
Le Vivilet <levivilet@pm.me>
2026-02-23 13:58:19 +0100

    fix: set skipLibCheck to false in tsconfig files for improved type checking

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,4 @@ import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 import * as tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
 
-export default [
-  ...config.default,
-  ...actions.default,
-  ...tsconfig.default,
-  {
-    rules: {
-      'tsconfig/dont-skip-lib-check': 'off',
-    },
-  },
-]
+export default [...config.default, ...actions.default, ...tsconfig.default]
