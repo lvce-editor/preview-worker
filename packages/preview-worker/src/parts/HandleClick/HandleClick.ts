bdfc708f47ddea7e7e6cbd88b1e45bd5925132ef
Le Vivilet <levivilet@pm.me>
2026-02-23 11:51:56 +0100

    feat: implement waitForClick functionality and integrate WaitForClickState management (#147)

--- packages/preview-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/preview-worker/src/parts/HandleClick/HandleClick.ts
@@ -1,9 +1,13 @@
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import * as CallAndUpdate from '../CallAndUpdate/CallAndUpdate.ts'
+import * as WaitForClickState from '../WaitForClickState/WaitForClickState.ts'
 
 export const handleClick = async (state: PreviewState, hdId: string, clientX: number, clientY: number): Promise<PreviewState> => {
   if (!hdId) {
     return state
+  }
+  if (WaitForClickState.has(state.uid)) {
+    WaitForClickState.resolve(state.uid)
   }
   const { x, y } = state
   const adjustedClientX = clientX - x
