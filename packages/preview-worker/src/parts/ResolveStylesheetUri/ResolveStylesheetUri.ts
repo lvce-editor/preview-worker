786401b32449baf553fa8f23204714bc86f4993b
Le Vivilet <levivilet@pm.me>
2026-02-23 16:36:31 +0100

    fix

--- packages/preview-worker/src/parts/ResolveStylesheetUri/ResolveStylesheetUri.ts
+++ packages/preview-worker/src/parts/ResolveStylesheetUri/ResolveStylesheetUri.ts
@@ -1,6 +1,6 @@
-import { hasScheme } from '../HasScheme/HasScheme.js'
-import { isBlockedStylesheetHref } from '../IsBlockedStylesheetHref/IsBlockedStylesheetHref.js'
-import { toBaseUrl } from '../ToBaseUrl/ToBaseUrl.js'
+import { hasScheme } from '../HasScheme/HasScheme.ts'
+import { isBlockedStylesheetHref } from '../IsBlockedStylesheetHref/IsBlockedStylesheetHref.ts'
+import { toBaseUrl } from '../ToBaseUrl/ToBaseUrl.ts'
 
 export const resolveStylesheetUri = (documentUri: string, href: string): string => {
   if (!href || href.startsWith('#') || isBlockedStylesheetHref(href)) {
