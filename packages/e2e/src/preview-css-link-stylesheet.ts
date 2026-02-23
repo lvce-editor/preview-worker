bbf24756a1fe08455186a1852f9b1ae12abd4fdb
Le Vivilet <levivilet@pm.me>
2026-02-23 15:30:25 +0100

    fix

--- packages/e2e/src/preview-css-link-stylesheet.ts
+++ packages/e2e/src/preview-css-link-stylesheet.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'preview.css-link-stylesheet'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
