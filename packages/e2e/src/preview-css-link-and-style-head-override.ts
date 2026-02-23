65dd04d0d51baeb9642d479acfdd6d1728ec2d5b
Le Vivilet <levivilet@pm.me>
2026-02-23 16:46:23 +0100

    refactor: remove skip flag from preview.css-link-and-style-head-override test (#172)

--- packages/e2e/src/preview-css-link-and-style-head-override.ts
+++ packages/e2e/src/preview-css-link-and-style-head-override.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'preview.css-link-and-style-head-override'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await Workspace.setPath(tmpDir)
