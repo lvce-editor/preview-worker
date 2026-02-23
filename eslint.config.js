0ae9394905e11b495e268b5b79eadec19e458473
Le Vivilet <levivilet@pm.me>
2026-02-23 16:33:30 +0100

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,11 @@ import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 import * as tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
 
-export default [{
-	ignores: ['**/coverage/**'],
-}, ...config.default, ...actions.default, ...tsconfig.default]
+export default [
+  {
+    ignores: ['**/coverage/**'],
+  },
+  ...config.default,
+  ...actions.default,
+  ...tsconfig.default,
+]
