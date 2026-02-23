ef28ce2c96e98b3171b6b8530019bc7b001cb882
Le Vivilet <levivilet@pm.me>
2026-02-23 14:23:36 +0100

    fix

--- packages/preview-worker/test/RenderEventListeners.test.ts
+++ packages/preview-worker/test/RenderEventListeners.test.ts
@@ -2,18 +2,43 @@ import { expect, test } from '@jest/globals'
 import * as DomEventListenersFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 
-test('renderEventListeners should return array with HandleClick and HandleInput event listeners', () => {
+test('renderEventListeners should return the expected event listeners', () => {
   const result = RenderEventListeners.renderEventListeners()
   expect(result).toEqual([
     {
       capture: true,
       name: DomEventListenersFunctions.HandleClick,
-      params: ['handleClick', 'event.target.dataset.id'],
+      params: ['handleClick', 'event.target.dataset.id', 'event.clientX', 'event.clientY'],
     },
     {
       capture: true,
       name: DomEventListenersFunctions.HandleInput,
       params: ['handleInput', 'event.target.dataset.id', 'event.target.value'],
+    },
+    {
+      capture: true,
+      name: DomEventListenersFunctions.HandleKeydown,
+      params: ['handleKeyDown', 'event.target.dataset.id', 'event.key', 'event.code'],
+    },
+    {
+      capture: true,
+      name: DomEventListenersFunctions.HandleKeyup,
+      params: ['handleKeyUp', 'event.target.dataset.id', 'event.key', 'event.code'],
+    },
+    {
+      capture: true,
+      name: DomEventListenersFunctions.HandleMousedown,
+      params: ['handleMousedown', 'event.target.dataset.id', 'event.clientX', 'event.clientY'],
+    },
+    {
+      capture: true,
+      name: DomEventListenersFunctions.HandleMousemove,
+      params: ['handleMousemove', 'event.target.dataset.id', 'event.clientX', 'event.clientY'],
+    },
+    {
+      capture: true,
+      name: DomEventListenersFunctions.HandleMouseup,
+      params: ['handleMouseup', 'event.target.dataset.id'],
     },
   ])
 })
