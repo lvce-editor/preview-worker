f72f37f7c70642d9c0166aee451261bd567573cc
Le Vivilet <levivilet@pm.me>
2026-02-23 15:30:38 +0100

    feature: add more e2e tests (#162)

---
+++ packages/e2e/src/preview-css-link-stylesheet-absolute-path-blocked.ts
@@ -0,0 +1,44 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'preview.css-link-stylesheet-absolute-path-blocked'
+
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, Workspace }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
+
+  const filePath = `${tmpDir}/preview-test-css-link-stylesheet-absolute-path-blocked.html`
+  const blockedCssPath = `${tmpDir}/preview-test-css-link-stylesheet-absolute-path-blocked.css`
+
+  const html = `<!DOCTYPE html>
+<html>
+<head>
+  <title>Absolute Path Stylesheet Blocked Test</title>
+  <style>
+    #target {
+      color: rgb(0, 128, 0);
+    }
+  </style>
+  <link rel="stylesheet" href="${blockedCssPath}">
+</head>
+<body>
+  <div id="target">Absolute path stylesheet should be blocked</div>
+</body>
+</html>`
+
+  const blockedCss = `#target {
+  color: rgb(255, 0, 0);
+}`
+
+  await FileSystem.writeFile(blockedCssPath, blockedCss)
+  await FileSystem.writeFile(filePath, html)
+  await Main.openUri(filePath)
+
+  await Command.execute('Layout.showPreview', filePath)
+
+  const previewArea = Locator('.Viewlet.Preview')
+  await expect(previewArea).toBeVisible()
+
+  const target = previewArea.locator('#target')
+  await expect(target).toBeVisible()
+  await expect(target).toHaveCSS('color', 'rgb(0, 128, 0)')
+}
---
+++ packages/e2e/src/preview-css-link-stylesheet-etc-passwd-blocked.ts
@@ -0,0 +1,42 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'preview.css-link-stylesheet-etc-passwd-blocked'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, Workspace }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
+
+  const filePath = `${tmpDir}/preview-test-css-link-stylesheet-etc-passwd-blocked.html`
+  const cssPath = `${tmpDir}/preview-test-css-link-stylesheet-etc-passwd-blocked.css`
+
+  const html = `<!DOCTYPE html>
+<html>
+<head>
+  <title>Etc Passwd Stylesheet Blocked Test</title>
+  <link rel="stylesheet" href="/etc/passwd">
+  <link rel="stylesheet" href="./preview-test-css-link-stylesheet-etc-passwd-blocked.css">
+</head>
+<body>
+  <div id="target">Preview should still render when /etc/passwd is referenced</div>
+</body>
+</html>`
+
+  const css = `#target {
+  color: rgb(0, 0, 255);
+}`
+
+  await FileSystem.writeFile(cssPath, css)
+  await FileSystem.writeFile(filePath, html)
+  await Main.openUri(filePath)
+
+  await Command.execute('Layout.showPreview', filePath)
+
+  const previewArea = Locator('.Viewlet.Preview')
+  await expect(previewArea).toBeVisible()
+
+  const target = previewArea.locator('#target')
+  await expect(target).toBeVisible()
+  await expect(target).toHaveCSS('color', 'rgb(0, 0, 255)')
+}
