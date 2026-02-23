dfffd12edd821a1751cb547f6e0f3a255ff6078f
Le Vivilet <levivilet@pm.me>
2026-02-23 13:42:12 +0100

    fix

--- packages/e2e/src/preview-canvas-basic.ts
+++ packages/e2e/src/preview-canvas-basic.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'preview.canvas-basic'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Workspace }) => {
   // Create a temporary HTML file with canvas element
   const tmpDir = await FileSystem.getTmpDir()
