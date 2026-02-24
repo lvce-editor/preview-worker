ba441014c8f3aaa3011f110cdd88a99652181c2f
Le Vivilet <levivilet@pm.me>
2026-02-24 01:18:00 +0100

    fix

--- packages/e2e/src/preview-css-link-stylesheet-live-update.ts
+++ packages/e2e/src/preview-css-link-stylesheet-live-update.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'preview.css-link-stylesheet-live-update'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ Command, Editor, expect, FileSystem, Locator, Main, SideBar, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
