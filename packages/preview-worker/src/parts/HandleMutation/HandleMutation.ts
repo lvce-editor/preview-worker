ebf73babe6aba20bfc995a406e8aece340ded848
Le Vivilet <levivilet@pm.me>
2026-02-25 11:14:31 +0100

    feat: add handleMutation and waitForMutation functions for improved state management

---
+++ packages/preview-worker/src/parts/HandleMutation/HandleMutation.ts
@@ -0,0 +1,11 @@
+import type { PreviewState } from '../PreviewState/PreviewState.ts'
+import { rerender } from '../Rerender/Rerender.ts'
+import * as WaitForClickState from '../WaitForClickState/WaitForClickState.ts'
+
+export const handleMutation = async (state: PreviewState): Promise<PreviewState> => {
+  const { uid } = state
+  if (WaitForClickState.has(uid)) {
+    WaitForClickState.resolve(uid)
+  }
+  return rerender(state)
+}
