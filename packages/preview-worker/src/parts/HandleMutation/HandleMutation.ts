2371941bbe60fd832d2ed5c1a96c328764e52b81
Le Vivilet <levivilet@pm.me>
2026-02-25 14:58:47 +0100

    refactor: update WaitForClickState registration to include action type for click and mutation handling (#204)

--- packages/preview-worker/src/parts/HandleMutation/HandleMutation.ts
+++ packages/preview-worker/src/parts/HandleMutation/HandleMutation.ts
@@ -4,8 +4,8 @@ import * as WaitForClickState from '../WaitForClickState/WaitForClickState.ts'
 
 export const handleMutation = async (state: PreviewState): Promise<PreviewState> => {
   const { uid } = state
-  if (WaitForClickState.has(uid)) {
-    WaitForClickState.resolve(uid)
+  if (WaitForClickState.has('mutation', uid)) {
+    WaitForClickState.resolve('mutation', uid)
   }
   return rerender(state)
 }
