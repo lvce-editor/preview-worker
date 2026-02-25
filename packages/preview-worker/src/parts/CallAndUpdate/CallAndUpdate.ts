ab6cae47cbdb7a132dd05d0351382900a34b06a5
Le Vivilet <levivilet@pm.me>
2026-02-25 14:38:58 +0100

    feat: refactor callAndUpdate to improve DOM comparison and add tests for parsedDom handling

--- packages/preview-worker/src/parts/CallAndUpdate/CallAndUpdate.ts
+++ packages/preview-worker/src/parts/CallAndUpdate/CallAndUpdate.ts
@@ -1,17 +1,20 @@
+import { isEqual } from 'lodash-es'
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import * as GetParsedNodesChildNodeCount from '../GetParsedNodesChildNodeCount/GetParsedNodesChildNodeCount.ts'
 
 export const callAndUpdate = async (state: PreviewState, method: string, ...args: readonly any[]): Promise<PreviewState> => {
-  const { sandboxRpc, uid } = state
+  const { parsedDom, parsedNodesChildNodeCount, sandboxRpc, uid } = state
   await sandboxRpc.invoke(method, uid, ...args)
   const serialized = await sandboxRpc.invoke('SandBox.getSerializedDom', uid)
-  const parsedDom = serialized.dom
+  const nextParsedDom = serialized.dom
+  const isEqualDom = isEqual(nextParsedDom, state.parsedDom)
+  const newParsedDom = isEqual ? parsedDom : nextParsedDom
   const { css } = serialized
-  const parsedNodesChildNodeCount = GetParsedNodesChildNodeCount.getParsedNodesChildNodeCount(parsedDom)
+  const newParsedNodesChildNodeCount = isEqualDom ? parsedNodesChildNodeCount : GetParsedNodesChildNodeCount.getParsedNodesChildNodeCount(parsedDom)
   return {
     ...state,
     css,
-    parsedDom,
-    parsedNodesChildNodeCount,
+    parsedDom: newParsedDom,
+    parsedNodesChildNodeCount: newParsedNodesChildNodeCount,
   }
 }
