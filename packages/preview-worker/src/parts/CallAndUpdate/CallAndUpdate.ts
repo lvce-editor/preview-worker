2a085e380e88ab4e7efb831654395f147ba0f708
Le Vivilet <levivilet@pm.me>
2026-02-25 15:20:57 +0100

    refactor: simplify callAndUpdate and handleMutation functions by removing unused code

--- packages/preview-worker/src/parts/CallAndUpdate/CallAndUpdate.ts
+++ packages/preview-worker/src/parts/CallAndUpdate/CallAndUpdate.ts
@@ -1,22 +1,7 @@
-import { isEqual } from 'lodash-es'
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
-import * as GetParsedNodesChildNodeCount from '../GetParsedNodesChildNodeCount/GetParsedNodesChildNodeCount.ts'
 
 export const callAndUpdate = async (state: PreviewState, method: string, ...args: readonly any[]): Promise<PreviewState> => {
-  const { parsedDom, parsedNodesChildNodeCount, sandboxRpc, uid } = state
+  const { sandboxRpc, uid } = state
   await sandboxRpc.invoke(method, uid, ...args)
-  const serialized = await sandboxRpc.invoke('SandBox.getSerializedDom', uid)
-  const nextParsedDom = serialized.dom
-  const isEqualDom = isEqual(nextParsedDom, state.parsedDom)
-  const newParsedDom = isEqualDom ? parsedDom : nextParsedDom
-  const { css } = serialized
-  const newParsedNodesChildNodeCount = isEqualDom
-    ? parsedNodesChildNodeCount
-    : GetParsedNodesChildNodeCount.getParsedNodesChildNodeCount(nextParsedDom)
-  return {
-    ...state,
-    css,
-    parsedDom: newParsedDom,
-    parsedNodesChildNodeCount: newParsedNodesChildNodeCount,
-  }
+  return state
 }
--- packages/preview-worker/src/parts/HandleMutation/HandleMutation.ts
+++ packages/preview-worker/src/parts/HandleMutation/HandleMutation.ts
@@ -1,11 +1,12 @@
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
-import { rerender } from '../Rerender/Rerender.ts'
+import { triggerRerender } from '../TriggerRerender/TriggerRerender.ts'
 import * as WaitForClickState from '../WaitForClickState/WaitForClickState.ts'
 
 export const handleMutation = async (state: PreviewState): Promise<PreviewState> => {
   const { uid } = state
+  await triggerRerender(state)
   if (WaitForClickState.has('mutation', uid)) {
     WaitForClickState.resolve('mutation', uid)
   }
-  return rerender(state)
+  return state
 }
