a25d2a0df28074c49d0feaab857459886e3d5e52
Le Vivilet <levivilet@pm.me>
2026-02-25 14:46:55 +0100

    refactor: streamline rerender function for improved readability and state handling (#196)

--- packages/preview-worker/src/parts/Rerender/Rerender.ts
+++ packages/preview-worker/src/parts/Rerender/Rerender.ts
@@ -2,27 +2,18 @@ import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import * as GetParsedNodesChildNodeCount from '../GetParsedNodesChildNodeCount/GetParsedNodesChildNodeCount.ts'
 
 export const rerender = async (state: PreviewState): Promise<PreviewState> => {
-  if (state.loadJavaScript) {
-    const { sandboxRpc, uid } = state
-    const serialized = await sandboxRpc.invoke('SandBox.getSerializedDom', uid)
-    const parsedDom = serialized.dom
-    const { css } = serialized
-    const parsedNodesChildNodeCount = GetParsedNodesChildNodeCount.getParsedNodesChildNodeCount(parsedDom)
-    return {
-      ...state,
-      css,
-      parsedDom,
-      parsedNodesChildNodeCount,
-    }
+  const { loadJavaScript, sandboxRpc, uid } = state
+  if (!loadJavaScript) {
+    return state
   }
-
-  // Create a new copy of parsedDom array to trigger diff
-  const parsedDom = [...state.parsedDom]
-
-  // Return a new state object with the copied parsedDom
-  // This will cause DiffItems.isEqual to return false since parsedDom reference changed
+  const serialized = await sandboxRpc.invoke('SandBox.getSerializedDom', uid)
+  const parsedDom = serialized.dom
+  const { css } = serialized
+  const parsedNodesChildNodeCount = GetParsedNodesChildNodeCount.getParsedNodesChildNodeCount(parsedDom)
   return {
     ...state,
+    css,
     parsedDom,
+    parsedNodesChildNodeCount,
   }
 }
