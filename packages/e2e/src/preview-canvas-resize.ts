99a9407bfd5f6cf4b9fa19fbe1b66096a58ba18d
Le Vivilet <levivilet@pm.me>
2026-03-18 18:05:23 +0100

    fix

--- packages/e2e/src/preview-canvas-resize.ts
+++ packages/e2e/src/preview-canvas-resize.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'preview.canvas-resize'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
