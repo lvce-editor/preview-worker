e810c593140eb9739707a10417e5108034c65ba9
Le Vivilet <levivilet@pm.me>
2026-07-26 12:52:59 +0000

    feature: use npm workspaces

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,23 +16,19 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x rollup -x lerna -x typescript`
-  SUB='All dependencies match the latest package versions'
-  if [[ "$OUTPUT" == *"$SUB"* ]]; then
-    echo "$OUTPUT"
-  else
-    rm -rf node_modules package-lock.json dist
-    npm install
-  fi
+  ncu -u -x @types/node -x rollup -x typescript
 }
 
-                                                       updateDependencies             &&
-cd packages/build                                   && updateDependencies && cd ../.. &&
-cd packages/e2e                                     && updateDependencies && cd ../.. &&
-cd packages/memory                                  && updateDependencies && cd ../.. &&
-cd packages/server                                  && updateDependencies && cd ../.. &&
-cd packages/preview-worker                       && updateDependencies && cd ../.. &&
+updateDependencies
+
+for packageDirectory in packages/*; do
+  (
+    cd "$packageDirectory"
+    updateDependencies
+  )
+done
+
+rm -rf node_modules packages/*/node_modules dist
+npm install
 
 echo "Great Success!"
-
-sleep 2
