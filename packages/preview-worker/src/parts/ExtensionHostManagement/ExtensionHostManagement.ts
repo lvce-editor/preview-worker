1f50f3c9e338f002598013fcafb0e524c9477c9c
Le Vivilet <levivilet@pm.me>
2026-07-30 13:23:48 +0000

    feature: add knip

--- packages/preview-worker/src/parts/ExtensionHostManagement/ExtensionHostManagement.ts
+++
@@ -1,6 +0,0 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-
-export const activateByEvent = (event: string, assetDir: string, platform: number): Promise<void> => {
-  // @ts-ignore
-  return RendererWorker.activateByEvent(event, assetDir, platform)
-}
