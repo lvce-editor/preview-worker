cfed8de55aa1423dc57c3c59995de8e4a724d6ed
Le Vivilet <levivilet@pm.me>
2026-08-16 12:29:28 +0000

    fix: forward preview resize to sandbox

--- packages/preview-worker/src/parts/Resize/Resize.ts
+++ packages/preview-worker/src/parts/Resize/Resize.ts
@@ -1,6 +1,8 @@
 import type { PreviewState } from '../PreviewState/PreviewState.ts'
 
-export const resize = (state: PreviewState, dimensions: any): PreviewState => {
+export const resize = async (state: PreviewState, dimensions: any): Promise<PreviewState> => {
+  const { sandboxRpc, uid } = state
+  await sandboxRpc.invoke('SandBox.resize', uid, dimensions)
   return {
     ...state,
     ...dimensions,
